273090a62e6089be9aa5d6bc0054dee986148125
이휘경 <77186025+LeeHwiGyoung@users.noreply.github.com>
2022-07-19 21:23:32 +0900

    Create  두 정수 사이의 합.js

---
+++ Level1/두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    var answer = 0;
+    if (a > b){
+        let c; 
+        c = a;
+        a = b;
+        b = c;
+    }
+    for(let i = a; i <= b ; i++)
+    {
+        answer += i;
+    }
+    return answer;
+}
