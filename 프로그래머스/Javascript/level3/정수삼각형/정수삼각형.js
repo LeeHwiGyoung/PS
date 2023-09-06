d34cc162785a731220bfe68799aca1e3180cfc1b
이휘경 <77186025+LeeHwiGyoung@users.noreply.github.com>
2023-09-06 15:05:23 +0900

    Create 정수삼각형.js

---
+++ 프로그래머스/Javascript/level3/정수삼각형/정수삼각형.js
@@ -0,0 +1,10 @@
+function solution(triangle) {
+    
+    for(let i = triangle.length - 1 ; i > 0 ; i--){
+        for(let j = 0 ; j < triangle[i].length - 1; j++){
+            triangle[i-1][j] = Math.max(triangle[i][j] + triangle[i-1][j] ,triangle[i][j+1] + triangle[i-1][j])
+        }
+    }
+    
+    return triangle[0][0];
+}
