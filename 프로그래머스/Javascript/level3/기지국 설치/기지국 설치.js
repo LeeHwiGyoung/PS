1597fb2b2616230656f02a470f16d1e8cc1ae704
이휘경 <77186025+LeeHwiGyoung@users.noreply.github.com>
2023-10-22 15:25:41 +0900

    Create 기지국 설치.js

---
+++ 프로그래머스/Javascript/level3/기지국 설치/기지국 설치.js
@@ -0,0 +1,21 @@
+/*
+    누적합 방식으로 포문 한 번 사용
+*/
+function solution(n, stations, w) {
+    var answer = 0;
+    let current = 0;
+    const distance = 2 * w + 1;
+    for(let i = 0 ; i < stations.length ; i++){
+        const gap = stations[i] -  w - current - 1;
+        current = stations[i] + w;
+        if(gap > 0) {
+            answer += Math.ceil(gap /distance);
+        }
+    }
+    
+    if(current < n){
+        answer += Math.ceil((n - current) / distance)
+    }
+
+    return answer;
+}
