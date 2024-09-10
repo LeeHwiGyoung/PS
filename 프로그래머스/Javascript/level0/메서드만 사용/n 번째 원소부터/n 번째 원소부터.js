9eb1898d7a12a6bff9cbb14d15adcbbcfa0903aa
HwiGyoung Lee <ter0000@naver.com>
2024-09-10 18:15:42 +0900

    slice

---
+++ 프로그래머스/Javascript/level0/메서드만 사용/n 번째 원소부터/n 번째 원소부터.js
@@ -0,0 +1,7 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181892
+//slice
+function solution(num_list, n) {
+    var answer = [];
+    answer = num_list.slice(n-1)
+    return answer;
+}
