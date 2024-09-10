78ceb290b6b4e545486a36548b7d4156ddca326a
HwiGyoung Lee <ter0000@naver.com>
2024-09-10 18:24:16 +0900

    slice

---
+++ 프로그래머스/Javascript/level0/메서드만 사용/문자열 뒤의 n글자/문자열 뒤의 n글자.js
@@ -0,0 +1,7 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181910
+//slice
+function solution(my_string, n) {
+    var answer = '';
+    answer = my_string.slice(-n)
+    return answer;
+}
