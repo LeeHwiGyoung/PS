2bb69e63d3776b00ff5226246bffd9c4a31d8841
HwiGyoung Lee <ter0000@naver.com>
2024-09-10 18:13:03 +0900

    slice 메소드

---
+++ 프로그래머스/Javascript/level0/메서드만 사용/n번째 원소까지/n번째 원소까지.js
@@ -0,0 +1,8 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181889
+//slice()
+
+function solution(num_list, n) {
+    var answer = [];
+    answer = num_list.slice(0 , n);
+    return answer;
+}
